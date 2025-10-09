1e7a367c159a49fb3bbbe46dc434e5c44916d5b8
Nando Erni <nando.erni@outlook.de>
2025-10-09 18:51:52 +0200

    make 0

--- src/pages/photoBooth.tsx
+++ src/pages/photoBooth.tsx
@@ -47,7 +47,7 @@ export default function PhotoBooth() {
 
     const interval = setInterval(() => {
       counter -= 1;
-      if (counter > 0) {
+      if (counter >= 0) {
         setCountdown(counter);
       } else {
         clearInterval(interval);
